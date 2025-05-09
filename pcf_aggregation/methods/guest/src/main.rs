178c59ccae67c93f49a7783c3af1127663f89c4b
lucasstuve <=l.stuve@campus.tu-berlin.de>
2025-05-09 12:40:42 +0200

    Generates a banal  pcf_proof, this is used in a 2nd step to generate a new proof

---
+++ pcf_aggregation/methods/guest/src/main.rs
@@ -0,0 +1,27 @@
+use risc0_zkvm::guest::env;
+use risc0_zkvm::Receipt;
+
+fn main() {
+    // TODO: Implement your guest code here
+
+    // read the input
+    let input: u32 = env::read();
+    let previous_id: [u32; 8] = env::read();
+    let previous_receipt: Receipt = env::read();
+
+    previous_receipt.verify(previous_id).unwrap();
+
+    let pcf_previous: u32 = previous_receipt.journal.decode().unwrap();
+
+    let pcf_new = pcf_previous + input;
+
+    println!(
+        "Generate proof that the new pcf (verified old + new pcf_activity is:){}",
+        pcf_new
+    );
+
+    // TODO: do something with the input
+
+    // write public output to the journal
+    env::commit(&pcf_new);
+}
